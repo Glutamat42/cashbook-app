c4345f361dc07eb7bcd93ef649726bd1794970e9
Glutamat42 <markus.heck13396@gmx.de>
2024-01-28 16:12:07 +0100

    move some variables to csv_import_store

--- lib/screens/csv_import_screen.dart
+++ lib/screens/csv_import_screen.dart
@@ -1,12 +1,13 @@
 import 'dart:typed_data';
 
 import 'package:cashbook/dialogs/new_category_dialog.dart';
-import 'package:cashbook/models/category.dart';
 import 'package:cashbook/services/locator.dart';
 import 'package:cashbook/stores/category_store.dart';
+import 'package:cashbook/stores/csv_import_store.dart';
 import 'package:cashbook/utils/csv_processor.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:logging/logging.dart';
 
 enum MergeStrategy { fillEmpty, overrideExisting }
@@ -18,22 +19,10 @@ class CsvImportScreen extends StatefulWidget {
 
 class _CsvImportScreenState extends State<CsvImportScreen> {
   final Logger _log = Logger('_CsvImportScreenState');
-  Uint8List? csvData;
   List<String>? csvHeaders;
-  String? selectedFileName;
-  Map<String, String?> fieldMappings = {};
-  Map<String, MergeStrategy> mergeStrategy = {
-    "description": MergeStrategy.fillEmpty,
-    "recipientSender": MergeStrategy.fillEmpty,
-    "date": MergeStrategy.fillEmpty,
-    "paymentMethod": MergeStrategy.fillEmpty,
-    "category": MergeStrategy.fillEmpty,
-    "category_default": MergeStrategy.fillEmpty,
-    "amount": MergeStrategy.fillEmpty,
-    "isIncome": MergeStrategy.fillEmpty,
-  };
   final _formKey = GlobalKey<FormState>();
   final CategoryStore _categoryStore = locator<CategoryStore>();
+  final CsvImportStore _csvImportStore = locator<CsvImportStore>();
   bool showMergeStrategyOptions = false;
   final List<String> incomeOptions = ["---", "Positive amount as Income", "Negative amount as Income"];
 
@@ -43,6 +32,7 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
     if (_categoryStore.categories.isEmpty) {
       _categoryStore.loadCategories();
     }
+    _csvImportStore.init();
   }
 
   @override
@@ -75,7 +65,7 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
       padding: const EdgeInsets.all(8.0),
       child: Column(
         children: [
-          Text("Select a template"),
+          const Text("Select a template"),
           const SizedBox(height: 10),
           Row(
             children: [
@@ -83,33 +73,48 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
                   onPressed: () {
                     String? defaultCategoryName;
                     try {
-                      defaultCategoryName = _categoryStore.categories.firstWhere((element) => element.name == "Imported").name;
+                      defaultCategoryName =
+                          _categoryStore.categories.firstWhere((element) => element.name == "Imported").name;
                     } catch (e) {
-                      _log.info("Failed to find category 'Imported'. This is expected if it was not yet manually created: ${e.toString()}");
+                      _log.info(
+                          "Failed to find category 'Imported'. This is expected if it was not yet manually created: ${e.toString()}");
                       defaultCategoryName = null;
                       // show snackbar
                       if (context.mounted) {
                         ScaffoldMessenger.of(context).showSnackBar(
                           const SnackBar(
-                            content: Text("Category 'Imported' not found. Please create it manually or select another category as default."),
+                            content: Text(
+                                "Category 'Imported' not found. Please create it manually or select another category as default."),
                           ),
                         );
                       }
                     }
-                    setState(() {
-                      fieldMappings = {
-                        "description": "Verwendungszweck",
-                        "recipientSender": "Beguenstigter/Zahlungspflichtiger",
-                        "date": "Buchungstag",
-                        "paymentMethod_default": "bank_transfer",
-                        "category": null,
-                        "category_default": defaultCategoryName,
-                        "amount": "Betrag",
-                        "isIncome": incomeOptions[1],
-                      };
-                    });
+                    _csvImportStore.fieldMappings = {
+                      "description": "Verwendungszweck",
+                      "recipientSender": "Beguenstigter/Zahlungspflichtiger",
+                      "date": "Buchungstag",
+                      "paymentMethod_default": "bank_transfer",
+                      "category": null,
+                      "category_default": defaultCategoryName,
+                      "amount": "Betrag",
+                      "isIncome": incomeOptions[1],
+                    };
                   },
-                  child: Text("Sparkasse")),
+                  child: const Text("Sparkasse")),
+              OutlinedButton(
+                  onPressed: () {
+                    _csvImportStore.fieldMappings = {
+                      "description": null,
+                      "recipientSender": null,
+                      "date": null,
+                      "paymentMethod_default": null,
+                      "category": null,
+                      "category_default": null,
+                      "amount": null,
+                      "isIncome": null,
+                    };
+                  },
+                  child: const Text("reset")),
             ],
           )
         ],
@@ -123,7 +128,7 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Text("Selected File: $selectedFileName"),
+            Observer(builder: (_) => Text("Selected File: ${_csvImportStore.selectedFileName}")),
             const SizedBox(height: 10),
             Row(
               children: [
@@ -215,16 +220,14 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
       }
       setState(() {
         csvHeaders = parsedData;
-        csvData = csvSourceData;
-        selectedFileName = result.files.single.name;
       });
+      _csvImportStore.csvData = csvSourceData;
+      _csvImportStore.selectedFileName = result.files.single.name;
     }
   }
 
   void updateFieldMapping(String field, String? value) {
-    setState(() {
-      fieldMappings[field] = value == "---" ? null : value;
-    });
+    _csvImportStore.fieldMappings[field] = value == "---" ? null : value;
   }
 
   Widget buildFieldMappingArea(BuildContext context) {
@@ -245,101 +248,88 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
     }
     categories.insert(0, "---");
 
-
-
     return Form(
       key: _formKey, // Define this key in your state class
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          buildDropdown("description", "Description", selectableHeaders, required: true),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["description"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["description"] = state;
-              });
-            }),
-          const Divider(),
-          buildDropdown("recipientSender", "Recipient/Sender", selectableHeaders, required: true),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["recipientSender"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["recipientSender"] = state;
-              });
-            }),
-          const Divider(),
-          buildDropdown("date", "Date", selectableHeaders),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["date"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["date"] = state;
-              });
-            }),
-          const Divider(),
-          buildDropdown("paymentMethod_default", "Payment Method (default)", ['cash', 'bank_transfer', 'not_payed']),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["paymentMethod"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["paymentMethod"] = state;
-              });
-            }),
-          const Divider(),
-          // buildDropdown("noInvoice", "No Invoice", selectableHeaders),
+      child: Observer(
+        builder: (_) => Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            buildDropdown("description", "Description", selectableHeaders, required: true),
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["description"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["description"] = state;
+              }),
+            const Divider(),
+            buildDropdown("recipientSender", "Recipient/Sender", selectableHeaders, required: true),
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["recipientSender"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["recipientSender"] = state;
+              }),
+            const Divider(),
+            buildDropdown("date", "Date", selectableHeaders),
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["date"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["date"] = state;
+              }),
+            const Divider(),
+            buildDropdown(
+                "paymentMethod_default", "Payment Method (default)", ['---', 'cash', 'bank_transfer', 'not_payed']),
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["paymentMethod"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["paymentMethod"] = state;
+              }),
+            const Divider(),
+            // buildDropdown("noInvoice", "No Invoice", selectableHeaders),
 
-          buildDropdown("category", "Category", selectableHeaders, enabled: true),
-          buildDropdown(
-            "category_default",
-            "Category (default)",
-            categories,
-            required: true,
-            trailing: _buildAddCategoryButton(context),
-          ),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["category"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["category"] = state;
-              });
-            }),
-          const Divider(),
-
-          // Special handling for the isIncome field
-          buildDropdown("amount", "Amount", selectableHeaders, required: true),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["amount"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["amount"] = state;
-              });
-            }),
-          const Divider(),
-          buildDropdown("isIncome", "Is Income", incomeOptions, required: true),
-          if (showMergeStrategyOptions)
-            _buildMergeStrategyRow(mergeStrategy["isIncome"]!, (MergeStrategy state) {
-              setState(() {
-                mergeStrategy["isIncome"] = state;
-              });
-            }),
-          const Divider(),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      _log.info("Form validated");
-                      // Handle form submission
-                    } else {
-                      _log.info("Form validation failed");
-                    }
-                  },
-                  child: const Text('Continue'),
-                ),
-              ],
+            buildDropdown("category", "Category", selectableHeaders, enabled: true),
+            buildDropdown(
+              "category_default",
+              "Category (default)",
+              categories,
+              required: true,
+              trailing: _buildAddCategoryButton(context),
             ),
-          ),
-        ],
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["category"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["category"] = state;
+              }),
+            const Divider(),
+
+            // Special handling for the isIncome field
+            buildDropdown("amount", "Amount", selectableHeaders, required: true),
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["amount"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["amount"] = state;
+              }),
+            const Divider(),
+            buildDropdown("isIncome", "Is Income", incomeOptions, required: true),
+            if (showMergeStrategyOptions)
+              _buildMergeStrategyRow(_csvImportStore.mergeStrategy["isIncome"]!, (MergeStrategy state) {
+                _csvImportStore.mergeStrategy["isIncome"] = state;
+              }),
+            const Divider(),
+
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _log.info("Form validated");
+                        // Handle form submission
+                      } else {
+                        _log.info("Form validation failed");
+                      }
+                    },
+                    child: const Text('Continue'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -365,9 +355,7 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
           context,
           onChanged: (int? newCategoryId) {
             if (newCategoryId != null) {
-              setState(() {
-                fieldMappings["category_default"] = _categoryStore.findCategoryName(newCategoryId);
-              });
+              _csvImportStore.fieldMappings["category_default"] = _categoryStore.findCategoryName(newCategoryId);
             }
           },
         ).showCategoryDialog();
@@ -396,7 +384,7 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
                 Expanded(
                   child: DropdownButtonFormField<String>(
                     isExpanded: true,
-                    value: fieldMappings[fieldValue] ?? "---",
+                    value: _csvImportStore.fieldMappings[fieldValue] ?? "---",
                     decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                     ),
@@ -407,9 +395,9 @@ class _CsvImportScreenState extends State<CsvImportScreen> {
                         : null,
                     validator: (value) {
                       if (required &&
-                          (!fieldMappings.containsKey(fieldValue) ||
-                              fieldMappings[fieldValue] == null ||
-                              fieldMappings[fieldValue]!.isEmpty)) {
+                          (!_csvImportStore.fieldMappings.containsKey(fieldValue) ||
+                              _csvImportStore.fieldMappings[fieldValue] == null ||
+                              _csvImportStore.fieldMappings[fieldValue]!.isEmpty)) {
                         return 'Please select a value.';
                       }
                       return null;
--- lib/stores/csv_import_store.dart
+++ lib/stores/csv_import_store.dart
@@ -1,3 +1,6 @@
+import 'dart:typed_data';
+
+import 'package:cashbook/screens/csv_import_screen.dart';
 import 'package:mobx/mobx.dart';
 
 part 'csv_import_store.g.dart';
@@ -5,5 +8,32 @@ part 'csv_import_store.g.dart';
 class CsvImportStore = _CsvImportStore with _$CsvImportStore;
 
 abstract class _CsvImportStore with Store {
+  @observable
+  Map<String, String?> fieldMappings = {};
 
+  @observable
+  Map<String, MergeStrategy> mergeStrategy = {};
+
+  @observable
+  String? selectedFileName;
+
+  @observable
+  Uint8List? csvData;
+
+  @action
+  void init() {
+    fieldMappings = {};
+    mergeStrategy = {
+      "description": MergeStrategy.fillEmpty,
+      "recipientSender": MergeStrategy.fillEmpty,
+      "date": MergeStrategy.fillEmpty,
+      "paymentMethod": MergeStrategy.fillEmpty,
+      "category": MergeStrategy.fillEmpty,
+      "category_default": MergeStrategy.fillEmpty,
+      "amount": MergeStrategy.fillEmpty,
+      "isIncome": MergeStrategy.fillEmpty,
+    };
+    selectedFileName = null;
+    csvData = null;
+  }
 }
